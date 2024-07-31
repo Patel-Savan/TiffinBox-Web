33b6683daeb1a1e4edf59fff123e2fdcc51946fd
KunjPathak12 <kunjpathak1212@gmail.com>
2024-07-30 21:02:24 -0300

    Backend Comments added -Kunj Pathak

--- backend/src/main/java/com/tiffinbox/backend/services/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/tiffinbox/backend/services/impl/AuthServiceImpl.java
@@ -1,3 +1,9 @@
+/**
+ * Author : Kunj Hiteshkumar Pathak
+ * Dalhousie Email : kn743706@dal.ca
+ * Commit Email : kunjpathak1212@gmail.com
+ */
+
 package com.tiffinbox.backend.services.impl;
 
 import com.tiffinbox.backend.dto.request.*;
